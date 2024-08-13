44d804e8e410762ce17b95bf3d2650247865e7dc
Sarkis <petrosyants.s@yandex.ru>
2024-08-13 07:49:44 +0300

    Добавьте файлы проекта.

---
+++ C# _while/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.JavaScript;
+public class MainClass()
+{
+    public static void Main()
+    {
+
+     
+
+
+
+
+
+        
+
+
+    }
+    }
