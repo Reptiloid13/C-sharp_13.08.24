eece108820e9829b11736acb186c337eb25216b1
Sarkis <petrosyants.s@yandex.ru>
2024-08-17 14:27:30 +0300

    Jane_27_05.03.1993

--- C# _while/Program.cs
+++ C# _while/Program.cs
@@ -1,28 +1,33 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Net.Security;
 using System.Runtime.ConstrainedExecution;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.JavaScript;
+using System.Security.Cryptography.X509Certificates;
 public class MainClass()
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
 
-       int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+        Console.WriteLine("Your name is {0} and age is {1}", name, age);
+        Console.WriteLine("Enter your birthdate: 05.03.1993 ");   
+       
+        Console.WriteLine("Your birthadate 05.03.1993");
+        Console.ReadKey();
 
-        if (num1 >= 100) 
-        {
-            num1 = num1 / 10 % 10;
-            Console.WriteLine(num1);
 
-        }
-        else
-        {
-            Console.WriteLine("NO");
-        }
 
 
 
 
     }
 }
+
+
+
+
