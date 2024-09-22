3d99be66993fd9c764da6f22734ac1721f0e809c
artur <example@email.com>
2024-09-22 09:09:58 +0500

    Point

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,12 +1,44 @@
-﻿var result = new Point() { X = 3, Y = 4 };
-Console.WriteLine(result.Distance());
+﻿var point = new Point(-99999999, 4);
 
-struct Point()
+Console.WriteLine(point.Distance());
+Console.WriteLine(point);
+Console.WriteLine(point.ToString());
+Console.WriteLine(Point.Distance(3, 4));
+
+struct Point
 {
-    public double X = 3;
-    public double Y = 4;
+    private double X = 0;
+    private double Y = 0;
 
-    public static double Distance(double X, double Y)
+    public override string ToString()
+    {
+        return base.ToString() + $"; x = {X}, y = {Y}";
+    }
+
+    public Point(double x, double y)
+    {
+        if (!IsRight(x, y))
+        {
+            Console.WriteLine($"Неверные координаты x = {x}, y = {y}");
+            x = 0;
+            y = 0;
+        }
+
+        X = x;
+        Y = y;
+    }
+
+    private bool IsRight(double x, double y)
+    {
+        return x > -99999 && y < 99999;
+    }
+
+    public static double Distance(double x, double y)
+    {
+        return Math.Sqrt(x * x + y * y);
+    }
+
+    public double Distance()
     {
         return Math.Sqrt(X * X + Y * Y);
     }
