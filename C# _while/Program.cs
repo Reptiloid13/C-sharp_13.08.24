914ff359cfe7c90536d4751eac2ddb4187146967
Sartkis_13 <petrosyants.s@yandex.ru>
2024-10-13 07:17:04 +0300

    Метод Clone() Протестировал у меня получилось, что он меняет переменную, соответвено могу сделать вывод, что он в Куче хранится в другой области памяти

--- C# _while/Program.cs
+++ C# _while/Program.cs
@@ -14,16 +14,12 @@ class Program
         //ArrayWithoutZero();
         //ArraySumOddNumbers();
         //SymbolsLetterOrNumver();
-        //Task1();
-        //StringTask();
-        //StringTask2();
-        //StringTask3();
-        // Array3();
         //ListEvenNumbers();
         // ListStringsWords();
         //ReverseArray();
-        DictionaryWords();
-        ListCapacity();
+        // DictionaryWords();
+        //ListCapacity();
+        Clone();
     }
 
     static void ArrayMultiply()
@@ -146,97 +142,6 @@ class Program
         }
     }
 
-    static void Task1()
-
-        //        На ввод поступает число.
-        //Программа должна вывести на консоль символ, соответствующий данному числу согласно таблице ASCII.
-
-    {
-        int number = Convert.ToInt32(Console.ReadLine());
-        var char1 = Convert.ToChar(number);
-
-        int number2 = Convert.ToInt32(Console.ReadLine());
-        var char2 = Convert.ToChar(number2);
-
-        Console.WriteLine(number + " " + number2);
-    }
-
-    static void StringTask()
-    {
-        // На ввод поступает строка состоящая из символов.
-        //Определите кол-во  цифр в данной строке.  (Строка без пробелов). 
-        //ac24e5sf0
-
-        var input = Console.ReadLine();
-        var numbersCount = 0;
-
-        for (int i = 0; i < input.Length; i++)
-        {
-            var symbol = input[i].ToString();
-            int num = 0;
-            var isInteger = int.TryParse(symbol, out num);
-            if (isInteger)
-            {
-                numbersCount++;
-            }
-        }
-
-        Console.WriteLine(numbersCount);
-    }
-
-    static void StringTask2()
-
-    {
-        //На ввод поступает строка, определите количество слов в строке
-
-        string s = Console.ReadLine();
-        int count = 1;
-        for (int i = 0; i < s.Length; i++)
-        {
-            if (s[i] == ' ' && i != 0 && s[i - 1] != ' ') count++;
-        }
-
-        Console.WriteLine(count);
-    }
-
-    static void StringTask3()
-    {
-        //        Напишите программу, которая запрашивает положительное натуральное целое число,
-        //        и выводит "YES", если в числе есть хотя бы один ноль, и "NO" иначе
-
-        int num1 = Convert.ToInt32(Console.ReadLine());
-        while (num1 > 0)
-        {
-            if (num1 % 10 == 0)
-            {
-                Console.Write("YES");
-                num1++;
-                break;
-            }
-            else
-            {
-                num1 = num1 / 10;
-                Console.Write("NO");
-            }
-        }
-    }
-
-    static void Array3()
-    {
-        //        На ввод подается размер массива и сам массив.
-
-        //Перезапишите и выведите массив на печать, где каждый элемент возведён в 3ю степень
-
-        int n = Convert.ToInt32(Console.ReadLine());
-        var items = Console.ReadLine().Split(' ');
-
-        for (int i = 0; i < n; i++)
-        {
-            var num = Convert.ToInt32(items[i]);
-            var isLast = i == n - 1;
-            Console.Write(Math.Pow(num, 3) + (isLast ? "" : " "));
-        }
-    }
 
 
     static void ListEvenNumbers()
@@ -341,4 +246,19 @@ class Program
             Console.WriteLine($"Capacity: {list.Capacity}");
         }
     }
-}
+
+    static void Clone()
+    {
+        //int[] array = { 5, 5, 9, 8, 3 };
+        //int result = array[0];
+        //int[] array2 = (int[])array.Clone();
+        //array2 = { 1, 2, 3 } ;
+
+        string original = "Big";
+        string cloneOriginal = (string)original.Clone();
+        cloneOriginal = original;
+        cloneOriginal = "Small";
+        Console.WriteLine($"{original}  получилось {cloneOriginal}");
+
+    }
+    }
