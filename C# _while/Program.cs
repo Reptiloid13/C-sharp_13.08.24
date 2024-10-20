ad2610d144cd23a304042ec6910a409bbdba1844
Sartkis_13 <petrosyants.s@yandex.ru>
2024-10-20 13:50:57 +0300

    Ошибка с аргументом

--- C# _while/Program.cs
+++ C# _while/Program.cs
@@ -18,19 +18,33 @@ class Program
                                            // person.SayHello("Иван");
         var x = Person.SayHello("Иван");
         Console.WriteLine(x);
+
+        Person person1 = new Person("Олег");
+        var y = Person.SayHello("Олег");
+        Console.WriteLine(y);
+        var count = Person.GetCount(); // А какой здесь нужен аргумент? 
+        Console.WriteLine($"Количество экземпляров {count}");
     }
 }
 
 class Person
 {
     public string Name;
+    public static int count = 0;
+
     public Person(string name)
     {
         Name = name;
+        count++;
     }
     public static string SayHello(string name)
     {
         return "Привет, меня зовут " + name;
+    }
+    public static int GetCount(int count)
+    {
+        return count;
+
     }
 }
 
