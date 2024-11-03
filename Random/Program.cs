1d6d64ba1b01906b57de333eda41a3c5db644e71
artur <example@email.com>
2024-11-03 09:46:29 +0500

    2

--- Random/Program.cs
+++ Random/Program.cs
@@ -6,18 +6,6 @@ class Program
 {
     public static void Main(string[] args)
     {
-        RandomTask1();
-    }
-
-    //Случайный цвет: Создайте массив с основными цветами(например, красный, зеленый, синий)
-    //     и напишите программу, которая выбирает случайный цвет и выводит его на экран.
-    public static void RandomTask1()
-    {
-        string[] colors = { "blue", "red", "green" };
-        var random = new Random();
-
-        var randomInt = random.Next(0, colors.Length); // 0, 1, 2
-        Console.WriteLine(colors[randomInt]);
     }
     //Подбрасывание монеты: Напишите программу, которая симулирует подбрасывание монеты 1млн раз и считает,
     //    сколько раз выпала орел, а сколько решка.
@@ -29,19 +17,16 @@ class Program
 
         for (int i = 0; i < 1000000; i++)
         {
-            bool result = true; //RandomNumber() == 0 ? true : false;
-
-
-            if (result)
+            if (GetRandomNumber() == 0)
                 heads++;
             else
                 tails++;
         }
     }
 
-    private static bool RandomNumber()
+    private static int GetRandomNumber()
     {
-        Random random = new Random();
-        return random.Next(2) == 0;
+        var random = new Random();
+        return random.Next(2);
     }
 }
