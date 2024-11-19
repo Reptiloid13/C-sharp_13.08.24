b43c9806ec94e8bead3b017e80659088dd49a0be
Sartkis_13 <petrosyants.s@yandex.ru>
2024-11-19 07:28:05 +0300

    1

--- WorkingWithFiles/FileSystemInMemory.cs
+++ WorkingWithFiles/FileSystemInMemory.cs
@@ -1,6 +1,48 @@
-﻿namespace WorkingWithFiles;
+﻿using System.Diagnostics.CodeAnalysis;
 
-public class FileSystemInMemory
+namespace WorkingWithFiles.Model;
+
+public class Model
 {
-    
+
+    public void GetCatalogs()
+    {
+        string dirName = @"C:\\";
+        if (Directory.Exists(dirName))
+        {
+            Console.WriteLine("Folders: ");
+            string[] dirs = Directory.GetDirectories(dirName);
+            foreach (string dir in dirs)
+                Console.WriteLine(dir);
+            Console.WriteLine();
+            Console.WriteLine("Files");
+            string[] files = Directory.GetFiles(dirName);
+            foreach (string s in files)
+                Console.WriteLine(s);
+        }
+    }
+    public void GetCatalogsVer2()
+    {
+        try
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(@"C://");
+            if (dirInfo.Exists)
+            {
+                Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+
+            }
+
+
+
+            DirectoryInfo newdirInfo = new DirectoryInfo(@"C:\Users\luft");
+            if (!newdirInfo.Exists)
+                newdirInfo.Create();
+
+            newdirInfo.CreateSubdirectory("NewFolder");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
 }
--- WorkingWithFiles/Program.cs
+++ WorkingWithFiles/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace WorkingWithFiles
+﻿using System;
+using System.IO;
+namespace WorkingWithFiles
 {
-    internal class Program
+    class FileWriter
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            var test = new Model.Model();
+            test.GetCatalogsVer2();
+
+
+
+            //string filePath = @"C:\Users\фвьшт\С_sharp\Students.txt"; ; // Укажем путь 
+            //if (!File.Exists(filePath)) // Проверим, существует ли файл по данному пути
+            //{
+            //    //   Если не существует - создаём и записываем в строку
+            //    using (StreamWriter sw = File.CreateText(filePath))  // Конструкция Using (будет рассмотрена в последующих юнитах)
+            //    {
+            //        sw.WriteLine("Олег");
+            //        sw.WriteLine("Дмитрий");
+            //        sw.WriteLine("Иван");
+            //    }
+            //}
+            //// Откроем файл и прочитаем его содержимое
+            //using (StreamReader sr = File.OpenText(filePath))
+            //{
+            //    string str = "";
+            //    while ((str = sr.ReadLine()) != null) // Пока не кончатся строки - считываем из файла по одной и выводим в консоль
+            //    {
+            //        Console.WriteLine(str);
+            //    }
+            //}
+
+
         }
     }
 }
