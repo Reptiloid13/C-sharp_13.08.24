8f7e18e899bd5700a157ca229782e520ab98a088
Sartkis_13 <petrosyants.s@yandex.ru>
2024-12-08 13:55:21 +0300

    Slove 1 task

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
+using SkillFactoryTask1;
 
 namespace SkillFactory;
 
@@ -10,27 +11,8 @@ class SkillFactoryTasks
 
     static void Main(string[] args)
     {
-        DirectoryInfo folder = new DirectoryInfo(@"C:\Users\фвьшт\Source\Repos\C-sharp_13.08.24\SkillFactory\Folders\\");
-        var span = TimeSpan.FromMinutes(30);
-        //var rootDirectory = new DirectoryInfo(folder);
-
-        if (!folder.Exists) throw new Exception("Папака не существует");
-
-        Console.WriteLine("Folders");
-        var dirs = folder.GetDirectories();
-
-        foreach (var dir in dirs)
-        {
-            Console.WriteLine(dir);
-            Console.WriteLine();
-        }
-        FileInfo[] files = folder.GetFiles();
-
-        foreach (var file in files)
-        {
-            Console.WriteLine();
-            Console.WriteLine(files);
-        }
+        var task1 = new Task1();
+        task1.DeleteUnusingFiles();
 
         //if(Directory.GetLastWriteTime(dir)< DateTime.Now.AddSeconds(-1))
         //   {
---
+++ SkillFactory/Task1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactoryTask1
+{
+    internal class Task1
+    {
+        //Напишите программу, которая чистит нужную нам папку от файлов  и папок, которые не использовались более 30 минут
+        public void DeleteUnusingFiles()
+        {
+
+            DirectoryInfo folder = new DirectoryInfo(@"C:\Users\фвьшт\Source\Repos\C-sharp_13.08.24\SkillFactory\Folders\");
+            var span = TimeSpan.FromMinutes(30);
+
+
+            if (!folder.Exists) throw new Exception("Папака не существует");
+
+            Console.WriteLine("Folders: ");
+            var dirs = folder.GetDirectories();
+
+            foreach (var dir in dirs)
+            {
+                Console.WriteLine(dir.FullName);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Files: ");
+
+            var files = folder.GetFiles("*", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                Console.WriteLine();
+                Console.WriteLine(file.FullName);
+
+                if (DateTime.Now - folder.LastAccessTime > span)
+                {
+                    folder.Delete(true);
+
+
+                    Console.WriteLine($" Folders Удалены : {folder}");
+                    Console.WriteLine();
+                    Console.WriteLine($"Files Удалены: {files}");
+                    Console.WriteLine();
+                }
+            }
+
+
+        }
+    }
+
+}
+
+
